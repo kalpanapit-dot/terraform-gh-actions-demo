56c5e392ed8c4266a41866a5544cb658e090d8f7
kalpana <kalpana.pit@sece.ac.in>
2026-07-06 13:35:59 +0530

    Initial Terraform S3 bucket config

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region to deploy in"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket (must be globally unique)"
+  type        = string
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Environment = "learning"
+    ManagedBy   = "github-actions"
+  }
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.demo_bucket.arn
+}
